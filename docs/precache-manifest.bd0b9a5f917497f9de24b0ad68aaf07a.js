eb76ea3f0dd2fbc7c91920d29302a100a1d31197
Huân LV <lehuan94cntt@gmail.com>
2018-10-17 10:06:31 +0700

    update pages git

---
+++ docs/precache-manifest.bd0b9a5f917497f9de24b0ad68aaf07a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e315665952b6d5ddf6d227b007af7f2",
+    "url": "/static/media/loading.9e315665.svg"
+  },
+  {
+    "revision": "2abf14f712eb7348e83d9057ce020ce2",
+    "url": "/static/media/casino-background.2abf14f7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29bf863793aa1abcb9d6",
+    "url": "/static/js/main.29bf8637.chunk.js"
+  },
+  {
+    "revision": "c041a32c508068f02331",
+    "url": "/static/js/1.c041a32c.chunk.js"
+  },
+  {
+    "revision": "29bf863793aa1abcb9d6",
+    "url": "/static/css/main.5ff8c9f3.chunk.css"
+  },
+  {
+    "revision": "78b1e2bb8ded4a7331fa73661db6bac0",
+    "url": "/index.html"
+  }
+];
